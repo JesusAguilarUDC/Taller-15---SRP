0481362a678ce2ae5c2460d565f17b0d9caa4dcf
JesusAguilarUDC <jaguilarn1@unicartagena.edu.co>
2024-11-20 17:07:56 -0500

    Se agregaron los ejemplos.

--- src/Ejercicio 3/AutenticacionUsuario.java
+++
@@ -1,7 +0,0 @@
-public class AutenticacionUsuario {
-
-    public boolean autenticar(Usuario usuario, String contrasenaIngresada) {
-        return usuario.getContrasena().equals(contrasenaIngresada);
-    }
-    
-}
